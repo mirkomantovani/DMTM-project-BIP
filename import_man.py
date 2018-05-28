b0bc30736e7ddbd4a7a40f3c4866b88278acc63b
nyxgear <dev@nyxgear.com>
2018-05-28 09:19:44 +0200

    reorganized preprocessing - date

--- import_man.py
+++ import_man.py
@@ -1,6 +1,15 @@
+from pprint import pprint
+
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib
 
 import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.stats import skew
+from scipy.stats import norm
+from scipy.stats.stats import pearsonr
+
+import warnings 
+warnings.filterwarnings("ignore")
