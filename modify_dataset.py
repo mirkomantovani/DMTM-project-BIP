47f9b9d786a737040939dad613d0c96d7759f28a
Mirko Mantovani <mirko.mantovani@mail.polimi.it>
2018-05-29 03:01:08 +0200

    Created modify_dataset.py utilities

---
+++ modify_dataset.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+def drop_zero_sales_rows(df):
+    df = df.drop(df[df.NumberOfSales == 0].index)
+    return df
+
+def drop_closed_store_rows(df):
+    df = df.drop(df[df.IsOpen == 0].index)
+    return df
+
+
+def get_fake_test(df):
+    return df.loc[((df['D_Month'] == 1) | (df['D_Month'] == 2)) & (df['D_Year'] == 2018)]
