5f6f80f10a1d0e71e70d7bbe079e6a2482f85ab8
nyxgear <dev@nyxgear.com>
2018-05-30 15:58:30 +0200

    error function fix!

--- BIP_error.py
+++ BIP_error.py
@@ -6,25 +6,25 @@ def get_BIP_error(df):
 	:param df: Data frame
 	:return: flaot Percentage of error
 	"""
-	error_evaluation_columns = [
-		'D_Month',
-		'Region',
-		'NumberOfSales',
-		'_NumberOfSales'
-	]
+	error_evaluation_columns = ['StoreID', 'D_Month', 'Region', 'NumberOfSales', '_NumberOfSales']
 
-	# Remove useless columns
+	# Remove useless columns and select all those required.
+	# Implicit check that all the required attributes to compute the error are
+	# present.
 	df = df[error_evaluation_columns]
 
 	# let's keep only march and april
 	# df = df.loc[df['D_Month'].isin([3, 4])] No. let's just evaluate months provided
 
+	# sum everything keeping distinguished: Month, Store and Region
+	df_sum_by_month = df.groupby(['D_Month', 'StoreID', 'Region']).sum()
+
 	# compute the difference between actual and predicted NumberOfSales and do the abs
-	df['abs_diff'] = df.apply(
-		lambda x: abs(x['NumberOfSales'] - x['_NumberOfSales']), axis=1)
+	df_sum_by_month['abs_diff'] = df_sum_by_month. \
+		apply(lambda x: abs(x['NumberOfSales'] - x['_NumberOfSales']), axis=1)
 
 	# Let's sum over the region
-	df_sums_by_region = df.groupby(['Region']).sum()
+	df_sums_by_region = df_sum_by_month.groupby(['Region']).sum()
 
 	# Divide the difference between actual and predicted NumberOfSales by the sum of actual
 	df_sums_by_region['E_r'] = \
