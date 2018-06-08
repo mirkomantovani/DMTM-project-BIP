c8abcf72f87d50961fb303d6b220091586960083
nyxgear <dev@nyxgear.com>
2018-06-08 16:35:57 +0200

    BIP error function as the one of the customer

--- BIP.py
+++ BIP.py
@@ -1,51 +1,3 @@
-def get_BIP_error(df):
-    """
-    Error on the predictions made. It's based on the BIP provided formula.
-
-    :param df: Data frame
-    :return: flaot Percentage of error
-    """
-    error_evaluation_columns = ['StoreID', 'D_Month', 'Region', 'NumberOfSales', '_NumberOfSales']
-
-    # Remove useless columns and select all those required.
-    # Implicit check that all the required attributes to compute the error are
-    # present.
-    df = df[error_evaluation_columns]
-
-    # let's consider only rows for which the store is open
-    # df = df[df.IsOpen == 1]  #Not needed anymore since we dropped it
-
-    # let's keep only march and april
-    # df = df.loc[df['D_Month'].isin([3, 4])] No. let's just evaluate months provided
-
-    # sum everything keeping distinguished: Month, Store and Region
-    df_sum_by_month = df.groupby(['D_Month', 'StoreID', 'Region']).sum()
-
-    # compute the difference between actual and predicted NumberOfSales and do the abs
-    df_sum_by_month['abs_diff'] = df_sum_by_month. \
-        apply(lambda x: abs(x['NumberOfSales'] - x['_NumberOfSales']), axis=1)
-
-    # Let's sum over the region
-    df_sums_by_region = df_sum_by_month.groupby(['Region']).sum()
-
-    # Divide the difference between actual and predicted NumberOfSales by the sum of actual
-    df_sums_by_region['E_r'] = \
-        df_sums_by_region['abs_diff'] / df_sums_by_region['NumberOfSales']
-
-    df_sums_by_region.head(20)
-
-    # Get the number of regions
-    N_regions = len(df.Region.unique())
-
-    print("Number of regions identified: {}".format(N_regions))
-
-    BIP_total_error = df_sums_by_region['E_r'].sum() / N_regions
-
-    print("BIP total error: {}".format(BIP_total_error))
-
-    return BIP_total_error
-
-
 def apply_BIP_submission_format(df, real_submit=False):
     """
     Given a dataframe with predictions, **NOT yet summed by month** it returns a dataframe which
@@ -95,3 +47,58 @@ def apply_BIP_submission_format(df, real_submit=False):
     df.rename(index=str, inplace=True, columns=columns_renamings)
 
     return df
+
+
+def get_BIP_error(df):
+    """
+    It's based on the BIP provided formula.
+    Given a dataframe with predictions, **NOT yet summed by month** it return the total error
+    done on predictions.
+
+    Required attributes in the dataframe:
+        ['StoreID', 'D_Month', 'NumberOfSales', '_NumberOfSales']
+
+    :param df: The data frame
+    :return: Dataframe in the submit format.
+    """
+    import pandas as pd
+    import numpy as np
+
+    required_attributes = ['StoreID', 'D_Month', 'NumberOfSales', '_NumberOfSales']
+
+    # create a copy of the dataframe to do not change the provided one
+    df = df.copy()
+
+    # Remove useless columns and select all the ones required.
+    # Implicit check that all the required columns are present.
+    df = df[required_attributes]
+
+    def get_regions():
+        # starting from the train set, get a dataframe which link the stores to their region
+        regions = pd.read_csv('./dataset/train.csv')
+        regions = regions[['StoreID', 'Region']]
+        regions.drop_duplicates(inplace=True)
+        return regions
+
+    # BIP provided code
+    def BIP_error(result, regions):
+        def regional_error(v):
+            y_true = v["Target"]
+            y_pred = v["NumberOfSales"]
+            return np.sum(np.abs(y_true - y_pred)) / np.sum(y_true)
+
+        def global_error(region_sums):
+            return np.mean(region_sums)
+
+        score = global_error(
+            pd.merge(result, regions, on="StoreID")[["Region", "Target", "NumberOfSales"]].groupby("Region").apply(
+                regional_error))
+        return score
+
+    result = apply_BIP_submission_format(df)
+    regions = get_regions()
+
+    BIP_total_error = BIP_error(result, regions)
+    print("BIP total error: {}".format(BIP_total_error))
+
+    return BIP_total_error
